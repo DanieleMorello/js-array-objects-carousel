54b8fa4692984c9691f26b88f8e866942b6bd01e
DanieleMorello <danielemorello@live.it>
2023-03-23 15:36:17 +0100

    Create a static card with html and css and add an array carousel on js

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/*
+  Dato un array di oggetti letterali con:
+    -url dell’immagine
+    -titolo
+    -descrizione
+
+  Creare un carosello come nella foto allegata.
+*/
+
+const carousel = [
+  {
+    url:'./assets/img/01.webp',
+    titolo: 'Spiderman',
+    descrizione: ''
+  },
+  {
+    url:'./assets/img/02.webp',
+    titolo: 'Ratchet & Clank',
+    descrizione: 'Platform-adventure game'
+  },
+  {
+    url:'./assets/img/03.webp',
+    titolo: 'Star wWrs',
+    descrizione: 'Fantasy movie'
+  },
+  {
+    url:'./assets/img/04.webp',
+    titolo: 'Stray',
+    descrizione: 'Adventur game'
+  },
+  {
+    url:'./assets/img/05.webp',
+    titolo: 'Avenger',
+    descrizione: 'Marvel heroes'
+  },
+]
